d027d7bb53d88c3847e96fb4607618e5ed0d8700
Molly Brown <mollybrown0516@gmail.com>
2021-08-25 12:43:41 -0400

    Completed HashTableII notes

--- HashTablesII.py
+++ HashTablesII.py
@@ -135,4 +135,173 @@ class HashTable:
             if self.get_load_factor() > 0.7:
                 self.resize(self.capacity * 2) # This is our get_load_factor function, that if the size of the table is greater than 0.7, we double our capacity
 
-    
+# Lecture Notes
+
+#Given a string, sort it in decreasing order based on the frequency of characters.
+
+# Example 1:
+# ​
+# ```plaintext
+# Input:
+# "free"
+# ​
+# Output:
+# "eefr"
+# ​
+# Explanation:
+# 'e' appears twice while 'f' and 'r' appear once.
+# So 'e' must appear before 'f' and 'r'. Therefore, "eerf" is also a valid answer.
+# ```
+# ​
+# Example 2:
+# ​
+# ```plaintext
+# Input:
+# "dddbbb"
+# ​
+# Output:
+# "dddbbb"
+# ​
+# Explanation:
+# Both 'd' and 'b' appear three times, so "bbbddd" is also a valid answer.
+# Note that "dbdbdb" is incorrect, as the same characters must be together.
+# ```
+# ​
+# Example 3:
+# ​
+# ```plaintext
+# Input:
+# "Bbcc"
+# ​
+# Output:
+# "ccBb"
+# ​
+# Explanation:
+# "ccbB" is also a valid answer, but "Bbcc" is incorrect.
+# Note that 'B' and 'b' are treated as two different characters.
+# ```
+# """
+def frequency_sort(s: str) -> str:
+    """
+    Inputs:
+    s -> str
+​
+    Output:
+    str
+    """
+    # Build our frequency_dict
+    freq_dict = {}
+    for char in s:
+        if char not in freq_dict:
+            freq_dict[char] = 1
+        else:
+            freq_dict[char] += 1
+
+    output_str = "".join(sorted(s, key=lambda x : freq_dict[x], reverse=True))
+    return output_str
+
+s = 'freeabbb'
+print(frequency_sort(s))
+
+#### HASH TABLES EXAMPLE #######################################################################################
+# my_dict = {}
+# my_dict = dict()
+
+class HashTable:
+    def __init__(self, size = 8):
+        self.storage = [None] * size
+        self.item_count = 0
+
+    def hash_func(self, string):
+        # Add up all the encoded values of
+        # the string chars
+        sum = 0
+        for char in string:
+            sum += ord(char)
+        return sum % len(self.storage)
+
+    def load_factor(self):
+        return self.item_count / len(self.storage)
+
+    def put(self, key, value):
+        # Hash the key
+        index = self.hash_func(key)
+
+        # check if self.storage at index, is None and store our value
+        if self.storage[index] is None:
+            self.storage[index] = [[key, value]]
+            self.item_count += 1
+            self.resize()
+            return
+        # Check if the key / value already exists
+        # loop through the "collision array"
+        for item in self.storage[index]:
+            # compare the item key, to our current key
+            if item[0] == key:
+                # Update the item value
+                item[1] = value
+                return
+        
+        # We definitely have a collision, because we passed the collision check above
+        self.storage[index].append([key, value])
+        self.item_count += 1
+        self.resize() #run the resize function to check if we need to resize
+
+
+    def get(self, key):
+        # Hash the key
+        index = self.hash_func(key)
+        # if inner array does not exist, return None
+        if self.storage[index] is None:
+            print("Key does not exist")
+            return
+        # search for our key
+        for item in self.storage[index]:
+            # compare the item key, to our current key
+            if item[0] == key:
+                # return the value
+                return item[1]
+        print("Key does not exist")
+        
+    def resize(self):
+        # Check if we actually should resize
+        if self.load_factor() < 0.7:
+            # we don't need to resize
+            return
+
+        # Lets resize our array
+        old_storage = self.storage
+        self.storage = [None] * (len(old_storage) * 2) #so we're multiplying the size of the array previously by 2
+        self.item_count = 0
+        # Go through ALL slots in old storage, and rehash everything!
+        for slot in old_storage:
+            # loop over items in the slots
+            if slot is None:
+                continue
+            for item in slot:
+                key = item[0]
+                value = item[1]
+                self.put(key, value)
+        
+
+    def __setitem__(self, key, value):
+        self.put(key, value)
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+
+my_dict = HashTable(8)
+my_dict['apple'] = 'is a fruit'
+my_dict['banana'] = 'banana is a fruit'
+my_dict['peach'] = 'is not a banana'
+my_dict['tomato'] = 'is red'
+my_dict['cucumber'] = 'is green'
+my_dict['plum'] = 'is great!'
+print(len(my_dict.storage))
+print(my_dict.load_factor())
+# print(my_dict['banana'])
+# print(my_dict['apple'])
+# print(my_dict['peach'])
+# print(my_dict.storage)
+# print(my_dict.load_factor())
